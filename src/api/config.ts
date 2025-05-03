0fbe74a166a1d87c143202213631b056629e470d
RANJANA SINGH <contactranjana03@gmail.com>
2025-05-03 19:09:34 +0530

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,9 +1,9 @@
 export const API_CONFIG = {
   BASE_URL: "https://api.openweathermap.org/data/2.5",
   GEO: "https://api.openweathermap.org/geo/1.0",
-  API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
+  API_KEY: a33d236e112c75205d9ad8902d91335c,
   DEFAULT_PARAMS: {
     units: "metric",
-    appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+    appid: a33d236e112c75205d9ad8902d91335c,
   },
 };
